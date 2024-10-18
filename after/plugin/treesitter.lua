319cac1d0a526d9e527d81ed1f2d3f95b33a953b
Tade Salverda <tadesalverda@outlook.com>
2024-10-18 11:57:05 +0200

    right parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "hyprlang", "c", "lua", "vim", "vimdoc", "query", "dart", "python", "javascript", "java", "c_sharp", "yaml", "dockerfile", "json", "make", "scala"},
+  ensure_installed = { "hyprlang", "c", "lua", "vim", "vimdoc", "query", "python", "yaml", "dockerfile", "json", "make", "latex"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
